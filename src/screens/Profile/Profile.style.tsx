a23330e6ece811048e20e3ba1d85ad5095245d2f
Roman Troshin <roman.troshin98@gmail.com>
2023-02-04 00:37:28 +0300

    feat: update Profile screen

--- src/screens/Profile/Profile.style.tsx
+++ src/screens/Profile/Profile.style.tsx
@@ -7,4 +7,39 @@ const Container = styled.View`
   padding: 24px 16px 0 16px;
 `;
 
-export default Container;
+const ItemContainer = styled.View`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const Wrapper = styled.View`
+  max-width: 300px;
+  width: 100%;
+`;
+
+const WrapperIcon = styled.View`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 24px;
+  height: 24px;
+`;
+
+const Subtitle = styled.Text`
+  font-size: ${({ theme }) => theme.typography.subtitle};
+  line-height: 24px;
+  color: ${({ theme }) => theme.colors.dark};
+  font-weight: 600;
+  margin-bottom: 4px;
+`;
+
+const StyledText = styled.Text`
+  font-size: ${({ theme }) => theme.typography.text};
+  line-height: 20px;
+  color: ${({ theme }) => theme.colors.grey};
+  font-weight: 400;
+`;
+
+export { Container, ItemContainer, Wrapper, WrapperIcon, Subtitle, StyledText };
--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -1,15 +1,70 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { memo } from 'react';
+import { TouchableHighlight, View } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import type { CompositeNavigationProp } from '@react-navigation/native';
+import type { BottomTabNavigationProp } from '@react-navigation/bottom-tabs';
+import type { StackNavigationProp } from '@react-navigation/stack';
 
 import userData from 'src/utils/constants';
 
 import UserInfo from 'src/components/UserInfo';
 
-import Container from './Profile.style';
+import type { StackParamList, TabParamList } from 'src/ts/types';
+
+import ArrowNextIcon from 'src/assets/icons/arrow-next.svg';
+
+import {
+  Container,
+  ItemContainer,
+  Wrapper,
+  Subtitle,
+  StyledText,
+  WrapperIcon,
+} from './Profile.style';
+
+type ProfileScreenNavigation = CompositeNavigationProp<
+  BottomTabNavigationProp<TabParamList, 'StackNavigation'>,
+  StackNavigationProp<StackParamList>
+>;
 
 function Profile() {
+  const { navigate } = useNavigation<ProfileScreenNavigation>();
+
+  const navigateOnPress = () => navigate('Payment');
+
   return (
     <Container>
       <UserInfo user={userData} />
+      <View>
+        <ItemContainer>
+          <Wrapper>
+            <Subtitle
+              style={{ fontFamily: 'Poppins-SemiBold' }}
+              numberOfLines={1}
+              ellipsizeMode="tail"
+            >
+              Payment options
+            </Subtitle>
+            <StyledText
+              style={{ fontFamily: 'Poppins-Regular' }}
+              numberOfLines={1}
+              ellipsizeMode="tail"
+            >
+              Find out the current, choose a new
+            </StyledText>
+          </Wrapper>
+          <TouchableHighlight
+            activeOpacity={0.6}
+            underlayColor="none"
+            onPress={navigateOnPress}
+          >
+            <WrapperIcon>
+              <ArrowNextIcon width={8} height={15} />
+            </WrapperIcon>
+          </TouchableHighlight>
+        </ItemContainer>
+      </View>
     </Container>
   );
 }
